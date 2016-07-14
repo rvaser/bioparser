ac41732f1a43c475a69991a914dd4ef5c57275c1
rvaser <robert.vaser@fer.hr>
2016-07-14 16:39:41 +0200

    Removed PAF format due to its design incompatibility

--- src/main.cpp
+++ src/main.cpp
@@ -34,25 +34,7 @@ class Overlap {
             uint32_t b_rc, uint32_t b_begin, uint32_t b_end, uint32_t b_length) :
                 id_(id), a_id_(a_id), b_id_(b_id), error_(error), minmers_(minmers),
                 a_rc_(a_rc), a_begin_(a_begin), a_end_(a_end), a_length_(a_length),
-                b_rc_(b_rc), b_begin_(b_begin), b_end_(b_end), b_length_(b_length),
-                a_name_(), b_name_(), matching_bases_(), overlap_length_(), quality_() {
-        }
-        Overlap(uint32_t id, const double* values, uint32_t values_length) :
-                id_(id), a_id_(values[0]), b_id_(values[1]), error_(values[2]), minmers_(values[3]),
-                a_rc_(values[4]), a_begin_(values[5]), a_end_(values[6]), a_length_(values[7]),
-                b_rc_(values[8]), b_begin_(values[9]), b_end_(values[10]), b_length_(values[11]),
-                a_name_(), b_name_(), orientation_(), matching_bases_(), overlap_length_(), quality_() {
-        }
-        Overlap(uint32_t id, const char* a_name, uint32_t a_name_length, uint32_t a_length,
-            uint32_t a_begin, uint32_t a_end, char strand, const char* b_name, uint32_t b_name_length,
-            uint32_t b_length, uint32_t b_begin, uint32_t b_end, uint32_t matching_bases,
-            uint32_t overlap_length, uint32_t quality) :
-                id_(id), a_id_(), b_id_(), error_(matching_bases / (double) overlap_length),
-                a_rc_(0), a_begin_(a_begin), a_end_(a_end), a_length_(a_length),
-                b_rc_(strand == '+' ? 0 : 1), b_begin_(b_begin), b_end_(b_end), b_length_(b_length),
-                a_name_(a_name, a_name_length), b_name_(b_name, b_name_length), orientation_(strand),
-                matching_bases_(matching_bases), overlap_length_(overlap_length),
-                quality_(quality) {
+                b_rc_(b_rc), b_begin_(b_begin), b_end_(b_end), b_length_(b_length) {
         }
 
         uint32_t id_;
@@ -68,13 +50,6 @@ class Overlap {
         uint32_t b_begin_;
         uint32_t b_end_;
         uint32_t b_length_;
-
-        std::string a_name_;
-        std::string b_name_;
-        char orientation_;
-        uint32_t matching_bases_;
-        uint32_t overlap_length_;
-        uint32_t quality_;
 };
 
 int main(int argc, char** argv) {
@@ -109,16 +84,6 @@ int main(int argc, char** argv) {
 
     gettimeofday(&stop, nullptr);
     fprintf(stderr, "MHAP: %.5lf\n", (((stop.tv_sec - start.tv_sec) * 1000000L + stop.tv_usec)
-        - start.tv_usec) / (double) 1000000);
-
-    gettimeofday(&start, nullptr);
-
-    auto preader = createPafReader<Overlap>(argv[4]);
-    std::vector<std::unique_ptr<Overlap>> test4;
-    preader->read_objects(test4, 10000000000000);
-
-    gettimeofday(&stop, nullptr);
-    fprintf(stderr, "PAF: %.5lf\n", (((stop.tv_sec - start.tv_sec) * 1000000L + stop.tv_usec)
         - start.tv_usec) / (double) 1000000);
 
     return 0;
